1aaca592d27ade99be5b41731291cad9bdc326bb
Artur DS <arturdsoma@gmail.com>
2020-11-19 23:13:59 +0300

    HW1 BASE JAVA.

--- src/Resume.java
+++ src/Resume.java
@@ -3,6 +3,14 @@
  */
 public class Resume {
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
     // Unique identifier
     String uuid;
 
